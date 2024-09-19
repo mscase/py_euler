901c1d049b92a82444cbd731fe064bf4e0527928
Matt Scase <matt.scase@gmail.com>
2024-09-18 20:14:55 -0400

    measure time to calculate with two different methods

--- problem1.py
+++ problem1.py
@@ -9,6 +9,19 @@ The sum of these multiples is 23.
 Find the sum of all the multiples of 3 or 5 below 1000
 """
 
+import time
+start = time.time()
 A = set([i for i in range(0,1000,3)])
 B = set([i for i in range(0,1000,5)])
+end = time.time()
 print (sum(A | B))
+print (f'with sets = {end - start}')
+
+total = 0
+start = time.time()
+for x in range(0, 1000):
+    if (x % 3 == 0 or x % 5 == 0):
+        total += x
+end = time.time()
+print (total)
+print (f'with loop = {end - start}')

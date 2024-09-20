5138da4a21cd5b59ed27e95b4f9e47376f4ed6ff
Matt Scase <matt.scase@gmail.com>
2024-09-19 21:45:14 -0400

    add comments, another method to calculate

--- problem15.py
+++ problem15.py
@@ -1,6 +1,18 @@
+#!/usr/bin/env python3 
+
+SUMMARY="""Lattice Paths"""
+
+"""
+Starting in the top left corner of a 2 x 2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.
+
+How many such routes are there through a 20 x 20 grid?
+"""
+
 import math
 
-# bin_coeffienct (n k) = ( n! / (k! * (n-k)!)
+# need to make 40 moves total, 20 right + 20 down
+
+# bin_coefficient (n k) = ( n! / (k! * (n-k)!)
 
 a = 20
 b = 20
@@ -11,4 +23,8 @@ n_fac = math.factorial(40)
 k_fac = math.factorial(20)
 nk_fac = k_fac
 
-print(n_fac / k_fac / nk_fac)
+print(int(n_fac / k_fac / nk_fac))
+
+# also
+
+print(int(math.comb(n, k)))

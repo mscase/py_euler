b86906721a5a187ff818b26b9c9e9260ba344054
Matt Scase <matt.scase@gmail.com>
2024-09-22 19:39:34 -0400

    removed list-ifcation, not required

--- problem20.py
+++ problem20.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python
 
+SUMMARY="""Factorial Digit Sum"""
+
+"""
+n! mean n * (n-1) * ... * 3 * 2 * 1.
+For example, 10! = 10 * 9 * ... 3 * 2 * 1 = 3628800
+and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+
+Find the sum of the digits in the number 100!
+"""
+
 import math
 
-chars=(list(str(math.factorial(100))))
+chars=(str(math.factorial(100)))
 total=0
-for x in chars:
-    total+=int(x)
+for c in chars:
+    total+=int(c)
 
 print (total)

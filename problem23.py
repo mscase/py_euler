0a4e587860dd41e5056151e565a9e8256ed03562
Matt Scase <matt.scase@gmail.com>
2024-09-22 21:07:31 -0400

    solved

--- problem23.py
+++ problem23.py
@@ -1,39 +1,52 @@
-#perfects=[]
-#for x in range(2, 28124, 2):
-#    div_x=[1]
-#    for y in range(2,x):
-#        if x % y == 0:
-#            div_x.append(y)
-#
-#    if x == sum(div_x): 
-#        print (x, div_x, sum(div_x))
-#        perfects.append(sum(div_x))
+#!/usr/bin/env python3
+
+SUMMARY="""Non-Abundant Sums"""
+
+"""
+A perfect number is a number for which the sum of its proper divisors is exactly equal to the number.
+For example, the sum of the proper divisors of 28 would be 1 + 2 + 4 + 7 + 14 = 28, 
+which means that 28 is a perfect number.
+
+A number n is called deficient if the sum of its proper divisors is less than n and it is called abundant if this sum exceeds n.
+
+As 12 is the smallest abundant number, 1 + 2 + 3 + 4 + 6 = 16, the smallest number that can be written as the sum of two abundant numbers is 24. 
+By mathematical analysis, it can be shown that all integers greater than 28123 can be written as the sum of two abundant numbers. 
+However, this upper limit cannot be reduced any further by analysis even though it is known that the greatest number that cannot be expressed as the sum of two abundant numbers is less than this limit.
+Find the sum of all the positive integers which cannot be written as the sum of two abundant numbers.
+"""
+
+# don't need this
 perfects = [6, 28, 496, 8128]
 print (perfects)
 
 abundants = []
 for x in range(2, 28124):
-    div_x=[1]
-    for y in range(2,x):
+    div_x={1}
+    for y in range(2,int(x**0.5)+1):
         if x % y == 0:
-            div_x.append(y)
+            div_x.add(y)
+            div_x.add(int(x/y))
     if sum(div_x) > x:
         abundants.append(x)
-print (abundants)
+#print (abundants)
+print (f'{len(abundants)} abundant numbers')
+num_abundants = len(abundants)
 
 all_nums = set([x for x in range(1,28124)])
-cannots = set()
-for n in range(1,28124):
-    print (n)
-    for a in abundants:
-        cannot = True
-        for b in abundants:
-            if a + b == n:
-                cannot = False
-                break
-        if cannot:
-            cannots.add(n)
+double_abundants = {num * 2 for num in abundants}
+all_nums = all_nums - double_abundants
 
-print (cannots)
+cannots = set()
+for num in all_nums:
+    cannot = True
+    limited_set = {x for x in abundants if x <= num}
+    for t_val in limited_set:
+        if (num - t_val in limited_set):
+            cannot = False
+            break
+    if cannot:
+        cannots.add(num)
+
+#print (cannots)
 print (sum(cannots))
 
